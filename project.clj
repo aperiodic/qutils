5ffbf83cc186fcf3dd53d81770383be824d21b8f
aperiodic <dlp@aperiodic.org>
2015-03-09 01:23:10 -0700

    Add project.clj (no deps necessary yet).

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject qutils "0.1.0-SNAPSHOT"
+  :description "Fundamental helpers for quil."
+  :url "https://github.com/aperiodic/qutils"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
