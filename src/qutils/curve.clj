412910d2f762852a52876a10f27e46d4aa8a445d
aperiodic <dlp@aperiodic.org>
2015-03-09 23:33:31 -0700

    Add `defcurve` macro, use to define linear curves.
    The `defcurve` macro allows one to easily define a curve by supplying
    just its fields and how to calculate its position as a function of t \in
    [0, 1]. It expands to a deftype that implements the Curve protocol and
    clojure.lang.Ifn, as well as a convenient constructor function.
    
    Rewrite the Linear curve definition to use this macro.

--- src/qutils/curve.clj
+++ src/qutils/curve.clj
@@ -1,28 +1,63 @@
-(ns qutils.curve)
+(ns qutils.curve
+  (:require [clojure.string :as str]))
 
 (defprotocol Curve
   "The protocol for curves used in animations. In order to implement a new kind of animation curve,
-  simply implement this protocol."
+  implement this protocol by using the `defcurve` macro below."
 
   (position-at [curve t]
     "Return the position of the curve at time `t`. The `t` argument will be clamped to remain
     between 0 and 1, inclusive."))
 
+(defmacro defcurve
+  "Define a new curve type that implements both the Curve protocol above, and clojure.lang.IFn
+  (using the position-at Curve protocol method).
 
-(deftype Linear
+  The `curve` argument is a symbol naming the curve. A deftype with the title-case version of the
+  name will be created (e.g. `Linear`), and a convenient constructor function with the lower-case
+  version will also be defined. (e.g. `linear`).
+
+  The `fields` are a vector of symbols defining the fields of the curve. These will be the fields of
+  the deftype and the arguments of the constructor function. You'll have access to them in the body
+  of the `position-fn`.
+
+  The `position-fn` is the implementation of the `position-at` method of the Curve protocol. It has
+  the same signature, and the `fields` of the curve are in-scope in its body.
+
+  Here's an example of defining a linear curve type using this macro:
+
+    (defcurve Linear
+      [lo hi]
+      (position-at
+        [_ t]
+        (let [diff (- hi lo)]
+          (+ lo (* t diff)))))
+  "
+  [curve fields position-fn]
+  (let [[_ args & body] position-fn
+        title-curve (-> (str/capitalize (name curve)) symbol)
+        lowercase-curve (-> (str/lower-case (name curve)) symbol)]
+    `(do
+       (deftype ~title-curve
+         ~fields
+         Curve
+         (~'position-at ~args
+           ~@body)
+
+         clojure.lang.IFn
+         (~'invoke [curve# t#] (~'position-at curve# t#))
+         (~'applyTo [curve# args#] (clojure.lang.AFn/applyToHelper curve# args#)))
+
+       (defn ~lowercase-curve
+         ~fields
+         (new ~title-curve ~@fields)))))
+
+(defcurve Linear
   [lo hi]
-  Curve
-  (position-at [curve t]
+  (position-at
+    [_ t]
     (case t
       (0 0.0) lo
       (1 1.0) hi
       (let [diff (- hi lo)]
-        (+ lo (* t diff)))))
-
-  clojure.lang.IFn
-  (invoke [curve t] (position-at curve t))
-  (applyTo [curve args] (clojure.lang.AFn/applyToHelper curve args)))
-
-(defn linear
-  [lo hi]
-  (Linear. lo hi))
+        (+ lo (* t diff))))))
